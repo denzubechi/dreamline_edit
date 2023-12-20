4bac7b26c81a3fa568362a245feaa45ee6f3528d
denzubechi <chukwumasamuel371@gmail.com>
2023-12-20 00:28:26 -0800

    edit

--- app/components/CTA.tsx
+++ app/components/CTA.tsx
@@ -5,11 +5,13 @@ import Demo from '../../media/1a35816f-99c5-4fad-bbce-150b3549f556.jpg';
 const CTA = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <Image
-        src={Demo}
-        className='object-cover w-full h-full'
-        alt='demo'
-      />
+      <div className='relative w-full h-full'>
+        <Image
+          src={Demo}
+          className='lg:object-cover'
+          alt='demo'
+        />
+      </div>
     </div>
   );
 };
